87be8c3d2c0d83e92bd213ca02fa65a5fc6d2739
Armando Gonzalez Hernandez <sixmit@hotmail.com>
2021-05-11 13:21:07 -0400

    Se agregaron propiedades autoimplementadas.

--- ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Entities/Coach.cs
+++ ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Entities/Coach.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoNaranja.Entities
 {
-    class Coach
+    public class Coach
     {
+        #region
+        [key]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+        [StringLength(30)]
+        public string LastName { get; set; }
+        [StringLength(30)]
+        public string PhoneNumber { get; set; }
+        [StringLength(30)]
+        public string CellPhoneNumber { get; set; }
+        [StringLength(30)]
+        public string Email { get; set; }
+        [StringLength(30)]
+        public string Photo { get; set; }
+        public string FullName { get { return $"{FirstName} { LastName}"; } }
+        #endregion
     }
 }
