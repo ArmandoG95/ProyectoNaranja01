8ebfce41d5c4a4630bf0f8be6ba9011e1cc583f3
KARLA PAULINA CABRERA ENRÍQUEZ <pcabrera.umad@gmail.com>
2021-05-11 12:58:54 -0500

    Se agrego el grid en la forma company y se modificaron las clase major y company

--- ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Entities/Company.cs
+++ ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Entities/Company.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProyectoNaranja.Entities
 {
-    class Company
+    public class Company
     {
+        #region Propiedades Autoimplementadas
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Address { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string PostalCode { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string PhoneNumber { get; set; }
+        public string Photo { get; set; }
+        [StringLength(50)] 
+        public string Email { get; set; }
+        [StringLength(50)] 
+        public string Website { get; set; }
+        #endregion
     }
 }
