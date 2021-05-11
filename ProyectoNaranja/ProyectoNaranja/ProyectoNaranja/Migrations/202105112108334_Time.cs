0189ddcff0b2e5fbc4fcc33eac408e3dae55ef2a
Armando Gonzalez Hernandez <sixmit@hotmail.com>
2021-05-11 17:10:36 -0400

    Se agregaron migraciones, inicio adviser, coach, company, contact, coordinator, major, student y time

---
+++ ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Migrations/202105112108334_Time.cs
@@ -0,0 +1,16 @@
+﻿namespace ProyectoNaranja.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Time : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
