6347b4af2d420f24299d1ed12a3628946bb06809
Emigdio Espinosa <millosespinosa@gmail.com>
2021-05-14 01:47:25 -0500

    Modificación de Form1 para convertirlo en un menú principal

--- ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Form1.Designer.cs
+++ ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Form1.Designer.cs
@@ -28,20 +28,282 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblSecciones = new MetroFramework.Controls.MetroLabel();
+            this.lblAdviser = new MetroFramework.Controls.MetroLabel();
+            this.btnAdviser = new MetroFramework.Controls.MetroButton();
+            this.lblPraticasProfesionales = new MetroFramework.Controls.MetroLabel();
+            this.btnContact = new MetroFramework.Controls.MetroButton();
+            this.lblContact = new MetroFramework.Controls.MetroLabel();
+            this.btnCompany = new MetroFramework.Controls.MetroButton();
+            this.lblCompany = new MetroFramework.Controls.MetroLabel();
+            this.btnCoach = new MetroFramework.Controls.MetroButton();
+            this.lblCoach = new MetroFramework.Controls.MetroLabel();
+            this.btnMajor = new MetroFramework.Controls.MetroButton();
+            this.lblMajor = new MetroFramework.Controls.MetroLabel();
+            this.btnStudent = new MetroFramework.Controls.MetroButton();
+            this.lblStudent = new MetroFramework.Controls.MetroLabel();
+            this.btnTime = new MetroFramework.Controls.MetroButton();
+            this.lblTime = new MetroFramework.Controls.MetroLabel();
+            this.btnCoordinator = new MetroFramework.Controls.MetroButton();
+            this.lblCoordinator = new MetroFramework.Controls.MetroLabel();
+            this.lblEquipoNaranja = new MetroFramework.Controls.MetroLabel();
+            this.btnExit = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
+            // 
+            // lblSecciones
+            // 
+            this.lblSecciones.AutoSize = true;
+            this.lblSecciones.Location = new System.Drawing.Point(390, 148);
+            this.lblSecciones.Name = "lblSecciones";
+            this.lblSecciones.Size = new System.Drawing.Size(71, 20);
+            this.lblSecciones.TabIndex = 0;
+            this.lblSecciones.Text = "Secciónes";
+            // 
+            // lblAdviser
+            // 
+            this.lblAdviser.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblAdviser.AutoSize = true;
+            this.lblAdviser.Location = new System.Drawing.Point(37, 200);
+            this.lblAdviser.Name = "lblAdviser";
+            this.lblAdviser.Size = new System.Drawing.Size(55, 20);
+            this.lblAdviser.TabIndex = 1;
+            this.lblAdviser.Text = "Adviser";
+            // 
+            // btnAdviser
+            // 
+            this.btnAdviser.Location = new System.Drawing.Point(26, 233);
+            this.btnAdviser.Name = "btnAdviser";
+            this.btnAdviser.Size = new System.Drawing.Size(75, 23);
+            this.btnAdviser.TabIndex = 2;
+            this.btnAdviser.Text = "Select";
+            this.btnAdviser.UseSelectable = true;
+            this.btnAdviser.Click += new System.EventHandler(this.btnAdviser_Click);
+            // 
+            // lblPraticasProfesionales
+            // 
+            this.lblPraticasProfesionales.AutoSize = true;
+            this.lblPraticasProfesionales.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lblPraticasProfesionales.Location = new System.Drawing.Point(343, 86);
+            this.lblPraticasProfesionales.Name = "lblPraticasProfesionales";
+            this.lblPraticasProfesionales.Size = new System.Drawing.Size(187, 25);
+            this.lblPraticasProfesionales.TabIndex = 3;
+            this.lblPraticasProfesionales.Text = "Practicas Profesionales";
+            this.lblPraticasProfesionales.Click += new System.EventHandler(this.metroLabel3_Click);
+            // 
+            // btnContact
+            // 
+            this.btnContact.Location = new System.Drawing.Point(335, 232);
+            this.btnContact.Name = "btnContact";
+            this.btnContact.Size = new System.Drawing.Size(75, 23);
+            this.btnContact.TabIndex = 5;
+            this.btnContact.Text = "Select";
+            this.btnContact.UseSelectable = true;
+            this.btnContact.Click += new System.EventHandler(this.btnContact_Click);
+            // 
+            // lblContact
+            // 
+            this.lblContact.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblContact.AutoSize = true;
+            this.lblContact.Location = new System.Drawing.Point(339, 200);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(56, 20);
+            this.lblContact.TabIndex = 4;
+            this.lblContact.Text = "Contact";
+            // 
+            // btnCompany
+            // 
+            this.btnCompany.Location = new System.Drawing.Point(229, 232);
+            this.btnCompany.Name = "btnCompany";
+            this.btnCompany.Size = new System.Drawing.Size(75, 23);
+            this.btnCompany.TabIndex = 7;
+            this.btnCompany.Text = "Select";
+            this.btnCompany.UseSelectable = true;
+            this.btnCompany.Click += new System.EventHandler(this.btnCompany_Click);
+            // 
+            // lblCompany
+            // 
+            this.lblCompany.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblCompany.AutoSize = true;
+            this.lblCompany.Location = new System.Drawing.Point(236, 200);
+            this.lblCompany.Name = "lblCompany";
+            this.lblCompany.Size = new System.Drawing.Size(68, 20);
+            this.lblCompany.TabIndex = 6;
+            this.lblCompany.Text = "Company";
+            // 
+            // btnCoach
+            // 
+            this.btnCoach.Location = new System.Drawing.Point(128, 233);
+            this.btnCoach.Name = "btnCoach";
+            this.btnCoach.Size = new System.Drawing.Size(75, 23);
+            this.btnCoach.TabIndex = 9;
+            this.btnCoach.Text = "Select";
+            this.btnCoach.UseSelectable = true;
+            this.btnCoach.Click += new System.EventHandler(this.btnCoach_Click);
+            // 
+            // lblCoach
+            // 
+            this.lblCoach.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblCoach.AutoSize = true;
+            this.lblCoach.Location = new System.Drawing.Point(138, 200);
+            this.lblCoach.Name = "lblCoach";
+            this.lblCoach.Size = new System.Drawing.Size(48, 20);
+            this.lblCoach.TabIndex = 8;
+            this.lblCoach.Text = "Coach";
+            // 
+            // btnMajor
+            // 
+            this.btnMajor.Location = new System.Drawing.Point(550, 233);
+            this.btnMajor.Name = "btnMajor";
+            this.btnMajor.Size = new System.Drawing.Size(75, 23);
+            this.btnMajor.TabIndex = 17;
+            this.btnMajor.Text = "Select";
+            this.btnMajor.UseSelectable = true;
+            this.btnMajor.Click += new System.EventHandler(this.btnMajor_Click);
+            // 
+            // lblMajor
+            // 
+            this.lblMajor.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblMajor.AutoSize = true;
+            this.lblMajor.Location = new System.Drawing.Point(566, 200);
+            this.lblMajor.Name = "lblMajor";
+            this.lblMajor.Size = new System.Drawing.Size(44, 20);
+            this.lblMajor.TabIndex = 16;
+            this.lblMajor.Text = "Major";
+            // 
+            // btnStudent
+            // 
+            this.btnStudent.Location = new System.Drawing.Point(651, 232);
+            this.btnStudent.Name = "btnStudent";
+            this.btnStudent.Size = new System.Drawing.Size(75, 23);
+            this.btnStudent.TabIndex = 15;
+            this.btnStudent.Text = "Select";
+            this.btnStudent.UseSelectable = true;
+            this.btnStudent.Click += new System.EventHandler(this.btnStudent_Click);
+            // 
+            // lblStudent
+            // 
+            this.lblStudent.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblStudent.AutoSize = true;
+            this.lblStudent.Location = new System.Drawing.Point(662, 200);
+            this.lblStudent.Name = "lblStudent";
+            this.lblStudent.Size = new System.Drawing.Size(56, 20);
+            this.lblStudent.TabIndex = 14;
+            this.lblStudent.Text = "Student";
+            // 
+            // btnTime
+            // 
+            this.btnTime.Location = new System.Drawing.Point(757, 232);
+            this.btnTime.Name = "btnTime";
+            this.btnTime.Size = new System.Drawing.Size(75, 23);
+            this.btnTime.TabIndex = 13;
+            this.btnTime.Text = "Select";
+            this.btnTime.UseSelectable = true;
+            this.btnTime.Click += new System.EventHandler(this.metroButton6_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(774, 200);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(40, 20);
+            this.lblTime.TabIndex = 12;
+            this.lblTime.Text = "Time";
+            // 
+            // btnCoordinator
+            // 
+            this.btnCoordinator.Location = new System.Drawing.Point(448, 233);
+            this.btnCoordinator.Name = "btnCoordinator";
+            this.btnCoordinator.Size = new System.Drawing.Size(75, 23);
+            this.btnCoordinator.TabIndex = 11;
+            this.btnCoordinator.Text = "Select";
+            this.btnCoordinator.UseSelectable = true;
+            this.btnCoordinator.Click += new System.EventHandler(this.btnCoordinator_Click);
+            // 
+            // lblCoordinator
+            // 
+            this.lblCoordinator.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblCoordinator.AutoSize = true;
+            this.lblCoordinator.Location = new System.Drawing.Point(448, 200);
+            this.lblCoordinator.Name = "lblCoordinator";
+            this.lblCoordinator.Size = new System.Drawing.Size(82, 20);
+            this.lblCoordinator.TabIndex = 10;
+            this.lblCoordinator.Text = "Coordinator";
+            // 
+            // lblEquipoNaranja
+            // 
+            this.lblEquipoNaranja.AutoSize = true;
+            this.lblEquipoNaranja.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.lblEquipoNaranja.Location = new System.Drawing.Point(370, 388);
+            this.lblEquipoNaranja.Name = "lblEquipoNaranja";
+            this.lblEquipoNaranja.Size = new System.Drawing.Size(91, 17);
+            this.lblEquipoNaranja.TabIndex = 18;
+            this.lblEquipoNaranja.Text = "Equipo Naranja";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(762, 379);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseSelectable = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 469);
+            this.ClientSize = new System.Drawing.Size(860, 425);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblEquipoNaranja);
+            this.Controls.Add(this.btnMajor);
+            this.Controls.Add(this.lblMajor);
+            this.Controls.Add(this.btnStudent);
+            this.Controls.Add(this.lblStudent);
+            this.Controls.Add(this.btnTime);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnCoordinator);
+            this.Controls.Add(this.lblCoordinator);
+            this.Controls.Add(this.btnCoach);
+            this.Controls.Add(this.lblCoach);
+            this.Controls.Add(this.btnCompany);
+            this.Controls.Add(this.lblCompany);
+            this.Controls.Add(this.btnContact);
+            this.Controls.Add(this.lblContact);
+            this.Controls.Add(this.lblPraticasProfesionales);
+            this.Controls.Add(this.btnAdviser);
+            this.Controls.Add(this.lblAdviser);
+            this.Controls.Add(this.lblSecciones);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Menú Principal";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroLabel lblSecciones;
+        private MetroFramework.Controls.MetroLabel lblAdviser;
+        private MetroFramework.Controls.MetroButton btnAdviser;
+        private MetroFramework.Controls.MetroLabel lblPraticasProfesionales;
+        private MetroFramework.Controls.MetroButton btnContact;
+        private MetroFramework.Controls.MetroLabel lblContact;
+        private MetroFramework.Controls.MetroButton btnCompany;
+        private MetroFramework.Controls.MetroLabel lblCompany;
+        private MetroFramework.Controls.MetroButton btnCoach;
+        private MetroFramework.Controls.MetroLabel lblCoach;
+        private MetroFramework.Controls.MetroButton btnMajor;
+        private MetroFramework.Controls.MetroLabel lblMajor;
+        private MetroFramework.Controls.MetroButton btnStudent;
+        private MetroFramework.Controls.MetroLabel lblStudent;
+        private MetroFramework.Controls.MetroButton btnTime;
+        private MetroFramework.Controls.MetroLabel lblTime;
+        private MetroFramework.Controls.MetroButton btnCoordinator;
+        private MetroFramework.Controls.MetroLabel lblCoordinator;
+        private MetroFramework.Controls.MetroLabel lblEquipoNaranja;
+        private MetroFramework.Controls.MetroButton btnExit;
     }
 }
 
--- ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Form1.cs
+++ ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Form1.cs
@@ -16,5 +16,63 @@ namespace ProyectoNaranja
         { 
             InitializeComponent();
         }
+
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton6_Click(object sender, EventArgs e)
+        {
+            FrmTime frmTime = new FrmTime();
+            frmTime.Show();
+        }
+
+        private void btnAdviser_Click(object sender, EventArgs e)
+        {
+            FrmAdviser frmAdviser = new FrmAdviser();
+            frmAdviser.Show();
+        }
+
+        private void btnCoach_Click(object sender, EventArgs e)
+        {
+            FrmCoach frmCoach = new FrmCoach();
+            frmCoach.Show();
+        }
+
+        private void btnCompany_Click(object sender, EventArgs e)
+        {
+            FrmCompany frmCompany = new FrmCompany();
+            frmCompany.Show();
+        }
+
+        private void btnContact_Click(object sender, EventArgs e)
+        {
+            FrmContact frmContact = new FrmContact();
+            frmContact.Show();
+        }
+
+        private void btnCoordinator_Click(object sender, EventArgs e)
+        {
+            FrmCoordinator frmCoordinator = new FrmCoordinator();
+            frmCoordinator.Show();
+        }
+
+        private void btnMajor_Click(object sender, EventArgs e)
+        {
+            FrmMajor frmMajor = new FrmMajor();
+            frmMajor.Show();
+        }
+
+        private void btnStudent_Click(object sender, EventArgs e)
+        {
+            FrmStudent frmStudent = new FrmStudent();
+            frmStudent.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
