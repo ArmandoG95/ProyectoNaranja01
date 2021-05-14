576ea17754c6bb1d57712731c25742eb118344a3
MOHAMED BUCHAHIM SIERRA <mohamed.buchahim@madero.edu.mx>
2021-05-13 22:27:17 -0500

    Funcionalidad Contact y migraciones

---
+++ ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/Migrations/202105140315296_Contact.cs
@@ -0,0 +1,22 @@
+﻿namespace ProyectoNaranja.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Contact : DbMigration
+    {
+        public override void Up()
+        {
+            
+            AlterColumn("dbo.Coordinators", "PhoneNumber", c => c.String(maxLength: 30));
+            AlterColumn("dbo.Students", "LastName", c => c.String(maxLength: 30));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Students", "LastName", c => c.String());
+            AlterColumn("dbo.Coordinators", "PhoneNumber", c => c.String());
+
+        }
+    }
+}
