8a6db382d9a9c8498b6a1e8875fd0850dceb4fc3
MOHAMED BUCHAHIM SIERRA <mohamed.buchahim@madero.edu.mx>
2021-05-13 22:38:48 -0500

    funcionalidad frm time_load

--- ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/FrmTime.cs
+++ ProyectoNaranja/ProyectoNaranja/ProyectoNaranja/FrmTime.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoNaranja.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,22 @@ namespace ProyectoNaranja
         public FrmTime()
         {
             InitializeComponent();
+        }
+
+        private void FrmTime_Load(object sender, EventArgs e)
+        {
+            {
+                using (DataContext dataContext = new DataContext())
+                {
+                    timeBindingSource.DataSource = dataContext.Times.ToList();
+                }
+                pnlDatos.Enabled = false;
+                Time time = timeBindingSource.Current as Time;
+                if (time != null && time.Photo != null)
+                    pctPhoto.Image = Image.FromFile(time.Photo);
+                else
+                    pctPhoto.Image = null;
+            }
         }
     }
 }
